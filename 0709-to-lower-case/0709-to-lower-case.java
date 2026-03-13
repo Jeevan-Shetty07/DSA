85f5137900603a174bdd89eb409364a19053e771
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-13 21:50:02 +0530

    Time: 0 ms (100%), Space: 42.8 MB (81.36%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -5,8 +5,8 @@ class Solution {
        for(int i=0;i<s.length();i++){
         if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
             arr[i]=(char)(arr[i]+32);
-        }else
-        arr[i]=s.charAt(i);
+        }
+       
        }
        return new String(arr);
     }
