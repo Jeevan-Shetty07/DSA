088414332370ffa32e9eae45eac821f02348f8a9
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-14 10:31:24 +0530

    Time: 5 ms (82.03%), Space: 46.9 MB (79.6%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-        int[] res=new int[nums.length];
+        // int[] res=new int[nums.length];
         Arrays.sort(nums);
         // int alice=0;
         // int bob=0;
@@ -8,9 +8,10 @@ class Solution {
         for (int i=0;i<nums.length;i+=2){
         //    alice=nums[i];
         //    bob=nums[i+1];
-           res[i]=nums[i+1];
-           res[i+1]=nums[i];
+            int temp=nums[i];
+           nums[i]=nums[i+1];
+           nums[i+1]=temp;
         }
-        return res;
+        return nums;
     }
 }
