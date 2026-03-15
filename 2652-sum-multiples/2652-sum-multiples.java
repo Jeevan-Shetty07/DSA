03ee2cc56d874426f988261badd5fc969fd9011a
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-15 17:03:01 +0530

    Time: 1 ms (100%), Space: 42.5 MB (87.64%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,11 +1,12 @@
 class Solution {
+      public int sum(int s,int n){
+           int  k=n/s;
+           return (s*k*(k+1))/2;
+
+        }
     public int sumOfMultiples(int n) {
-        int sum=0;
-           for(int i=3;i<=n;i++){
-               if(i%3==0 || i%5==0 || i%7==0){
-                   sum+=i;
-               }
-           }
-           return sum;
+      
+
+        return sum(3,n)+sum(5,n)+sum(7,n)-sum(15,n)-sum(21,n)-sum(35,n)+sum(105,n);
     }
 }
