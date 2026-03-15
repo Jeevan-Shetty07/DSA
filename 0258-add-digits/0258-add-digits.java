5c2efc1adf73cb879ca9e0a392437228b82a3f1c
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-15 20:41:51 +0530

    Time: 1 ms (97.97%), Space: 42.4 MB (89.26%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=999;
+        while(sum>9){
+            sum=0;
+            while(num>0){
+                int dig=num%10;
+                sum+=dig;
+                num=num/10;
+            }
+            num=sum;
+        }
+        return sum;
+    }
+}
