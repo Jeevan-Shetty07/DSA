9994844912621af733ee7fb3bd6f8ab7828c83f0
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-15 19:58:50 +0530

    Time: 1 ms (99.8%), Space: 43 MB (98.7%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,24 +1,28 @@
 class Solution {
     public int calPoints(String[] operations) {
         int pos=0;
+           int sum=0;
         int[] elements=new int[operations.length];
         for(String s:operations){
             if(s.equals("+")){
-               elements[pos++]=elements[pos-2]+elements[pos-3];
+               elements[pos]=elements[pos-1]+elements[pos-2];
+               sum+=elements[pos++];
             }else if(s.equals("D")){
                 elements[pos]=elements[pos-1]*2;
+                sum+=elements[pos];
                 pos++;
             }else if(s.equals("C")){
-                elements[--pos]=0;
+                sum-=elements[--pos];
+                elements[pos]=0;
                 
             }else{
-                elements[pos++]=Integer.parseInt(s);
+                elements[pos]=Integer.parseInt(s);
+                sum+=elements[pos];
+                pos++;
             }
         }
-        int sum=0;
-        for(int ele : elements){
-            sum+=ele;
-        }
+     
+       
         return sum;
     }
 }
