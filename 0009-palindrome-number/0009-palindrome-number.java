8285b22d4a78bb43fbef2cda1da9dfbe43d2bda4
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-17 21:43:28 +0530

    Time: 5 ms (79.81%), Space: 45.9 MB (84.61%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        int rev = 0, bit, temp = x;
+        boolean flag;
+        while (temp > 0) {
+            bit = temp % 10;
+            rev = (rev * 10) + bit;
+            temp = temp / 10;
+        }
+        if (rev == x) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
