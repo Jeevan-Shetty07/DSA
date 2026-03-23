144ce53cf024faf3b93696489f32dc3376050e4e
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-03-23 22:12:51 +0530

    Time: 22 ms (6.82%), Space: 52.6 MB (59.92%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        int n=nums.length/3;
+        List<Integer> res=new ArrayList<>();
+
+        if(nums.length<3){
+            for(int num:nums){
+                if(!res.contains(num)){
+                res.add(num);
+                }
+            }
+            return res;
+        }
+        Hashtable<Integer ,Integer> freq=new Hashtable<>();
+        for(int i=0;i<nums.length;i++){
+            if(freq.containsKey(nums[i])){
+                freq.put(nums[i],freq.get(nums[i])+1);
+                if(freq.get(nums[i])>n && !res.contains(nums[i])){
+                    res.add(nums[i]);
+
+                }
+            }else{
+                freq.put(nums[i],1);
+            }
+        }
+        return res;
+    }
+}
