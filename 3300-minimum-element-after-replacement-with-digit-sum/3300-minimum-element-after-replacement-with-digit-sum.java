2d9f86aca5338b275c20fe98c5bc70cee79bec6b
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-06-15 20:44:44 +0530

    Time: 1 ms (99.99%), Space: 44.7 MB (68.99%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minElement(int[] nums) {
-        int min=Integer.MAX_VALUE,sum=0 ,pos=0;
+        int min=Integer.MAX_VALUE,sum=0 ;
         for (int i:nums) {
                sum=0;
                while(i>0){
                     sum+=i%10;
                     i=i/10;
                }
-               nums[pos]=sum;
                min=Math.min(min,sum);
-
         }
         return min;
     }
