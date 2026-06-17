6fa8b2373c9a44bdd7daeeda86d6fabeee11d4da
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-06-17 21:11:17 +0530

    Time: 4 ms (6.15%), Space: 48.3 MB (60.62%) - LeetHub

---
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int deg;
+        int[] res=new int[matrix.length];
+        int resc=0;
+        for(int i=0;i<matrix.length;i++){
+            deg=0;
+            for(int j=0;j<matrix.length;j++){
+                if(matrix[j][i]==1){
+                      deg++;
+                }
+            }
+            res[resc++]=deg;
+        }
+        return res;
+    }
+}
