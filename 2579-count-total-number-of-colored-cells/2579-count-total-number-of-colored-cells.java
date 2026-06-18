b4dbe46c0b1ed17e815f939fa77d3bd05731ce13
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-06-18 15:21:31 +0530

    Time: 0 ms (100%), Space: 42.2 MB (23.56%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,12 +1,6 @@
 class Solution {
     public long coloredCells(int n) {
-        if(n==1){
-            if(n==0){
-                return 0;
-            }
-            return n=1;
-        }
-        ;
+     
         return  ((4*((long)n-1)*((long)n))/2)+1;
     }
 }
