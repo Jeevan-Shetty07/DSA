430612a4c10d5257d2f8af2fe2a9113b0a51e779
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-06-18 10:23:07 +0530

    Time: 2 ms (4.76%), Space: 44.7 MB (87.91%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -3,11 +3,12 @@ class Solution {
           double minangle=((double)minutes/5d)*30;
           double hours=(double)hour*30d>=360d?360-(double)hour*30d:(double)hour*30d;
           double hourangle=hours+(minutes*0.5d);
+          System.out.print(hourangle);
           if(Math.abs(minangle-hourangle)<360d-Math.abs(minangle-hourangle)){
             return  Math.abs(minangle-hourangle);
           }else{
             return 360d-Math.abs(minangle-hourangle);
           }
-        //  return 0;
+        
     }
 }
