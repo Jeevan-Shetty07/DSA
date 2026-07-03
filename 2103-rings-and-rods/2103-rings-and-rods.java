45bcfc2dc774b623776dcc8cf2e03f24afb595d2
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-07-03 11:37:09 +0530

    Time: 1 ms (60%), Space: 42.8 MB (46.97%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countPoints(String rings) {
-        int count=0;
-      
+        int count = 0;
+        int length = rings.length();
         for (int i = 0; i < 10; i++) {
-              boolean r = false, b = false, g = false;
-            for (int j = rings.length() - 1; j > 0; j -= 2) {
-               char pole=rings.charAt(j),col=rings.charAt(j-1);
-                if (pole == (char)i+'0') {
+            boolean r = false, b = false, g = false;
+            for (int j = length - 1; j > 0; j -= 2) {
+                char pole = rings.charAt(j), col = rings.charAt(j - 1);
+                if (pole == (char) i + '0') {
                     if (col == 'B')
                         b = true;
                     else if (col == 'G')
@@ -15,13 +15,13 @@ class Solution {
                         r = true;
 
                 }
+                if (r && g && b) {
+                    count++;
+                    break;
+                }
             }
             // System.out.println(r+" "+g+" "+b);
-            if(r && g && b){
-                count++;
-            }
-               
-           
+
         }
         return count;
 
