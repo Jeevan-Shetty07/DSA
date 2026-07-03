e8b3e2ac559b6be67d483c15af979021c775cb44
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-07-03 20:43:40 +0530

    Time: 1 ms (95.94%), Space: 42.2 MB (45.43%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -4,9 +4,10 @@ class Solution {
         if(num==1){
             return false;
         }
-        for(int i=2;i<=num/2;i++){
+        for(int i=2;i*i<num;i++){
             if(num%i==0){
                 sum+=i;
+                sum+=num/i;
             }
         }
         return sum==num;
