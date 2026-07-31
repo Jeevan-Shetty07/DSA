cce5f9f28f2a94259e7bdd867d4680de0ed64d74
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-07-31 11:50:04 +0530

    Time: 9 ms (94.22%), Space: 47.7 MB (95.95%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,21 +1,20 @@
 class Solution {
     public int minimumPushes(String word) {
-        Integer[] freq = new Integer[26];
-        Arrays.fill(freq, 0);
+        int[] freq = new int[26];
+        // Arrays.fill(freq, 0);
         for (char c : word.toCharArray()) {
 
             freq[c - 'a']++;
         }
-        Arrays.sort(freq, Collections.reverseOrder());
-        int count = 0, pos = 1;
-        for (int i = 0; i < freq.length; i++) {
+        Arrays.sort(freq);
+        int count = 0, num = 0;
+
+        for (int i = 25; i >= 0; i--) {
             if (freq[i] == 0) {
                 return count;
             }
-            if (i % 8 == 0 && i!=0) {
-                pos++;
-            }
-            count += freq[i] * pos;
+            count += freq[i] * (num / 8 + 1);
+            num++;
 
         }
         return count;
