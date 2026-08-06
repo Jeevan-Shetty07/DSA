03a13d1963c1143e54d088895e2eacb7aa14d046
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-08-06 20:49:59 +0530

    Time: 1 ms (100%), Space: 42.8 MB (14.66%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -7,7 +7,7 @@ class Solution {
             return n+(t-(n%t))>=10?10:n+(t-(n%t));
          }
          int ldig=n%10,fdig=n/10;
-         System.out.println(fdig+" "+ldig);
+        //  System.out.println(fdig+" "+ldig);
          for(int i=ldig;i<10;i++){
             if((i*fdig)%t==0){
                 return (fdig*10)+i;
