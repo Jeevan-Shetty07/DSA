e5bf1d221d5f9851aca219234619ce43b1f8e743
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-08-10 20:45:22 +0530

    Time: 9 ms (31.78%), Space: 44.1 MB (20.83%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> hm = new HashMap<>();
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            hm.put(s.charAt(i), i);
+        }
+        int lpos = 0, pos = 0, count = 0,prev=0;
+        while (pos < s.length()) {
+             lpos =(int) hm.get(s.charAt(pos));
+            pos++;
+            while (pos <= lpos) {
+                if (hm.get(s.charAt(pos)) > lpos) {
+                    lpos = hm.get(s.charAt(pos));
+                }
+                pos++;
+            }
+            al.add(pos -prev);
+            prev=pos;
+            count++;
+
+        }
+        return al;
+
+    }
+}
