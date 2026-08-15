cca6d0e8cfcbff0f62cea34a58f0aa36c0156084
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-08-15 20:07:17 +0530

    Time: 609 ms (5.06%), Space: 48 MB (24.01%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length<=1){
+            return true;
+        }
+        int end=nums.length-1,start=end-1;
+       for(int i=start;i>=0;i--){
+            if(i+nums[i]>=end){
+                end=i;
+            }
+            System.out.println(end);
+       }
+     return end==0?true:false;
+    }
+}
