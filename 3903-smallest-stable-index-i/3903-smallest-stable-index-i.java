24d0c4047527b52f0c4e66cd8c563e2eed9bce57
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-09-04 20:13:34 +0530

    Time: 42 ms (4.47%), Space: 46.5 MB (15.74%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -12,7 +12,7 @@ class Solution {
         }
         for(int i=n-2;i>=0;i--){
               minsum[i]=Math.min(nums[i],minsum[i+1]);
-                System.out.println(maxsum[i]);
+                // System.out.println(maxsum[i]);
         }
         
         for(int i=0;i<nums.length;i++){
