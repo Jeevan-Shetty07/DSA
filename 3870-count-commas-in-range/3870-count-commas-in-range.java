9551fa5b149a2664825c2eb5412c0d7ea2c284cc
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-09-08 05:59:52 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -2,10 +2,6 @@ class Solution {
     public int countCommas(int n) {
         if (n < 1000)
             return 0;
-        if (n <= 999999)
-            return n - 999;
-        // int comma=9999-999;
-        // if(n<=99999)
-        return 0;
+        return n - 999;
     }
 }
