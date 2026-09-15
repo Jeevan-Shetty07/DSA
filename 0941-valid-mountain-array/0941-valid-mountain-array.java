282cd55566a395f150401dab2cdd7f033e8840eb
Jeevan Shetty <jeevanshetty9481@gmail.com>
2026-09-15 22:45:43 +0530

    Time: 1 ms (99.92%), Space: 47.7 MB (16.54%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -6,22 +6,22 @@ class Solution {
 
         int i = 1;
 
-        // Increasing part
+      
         for (; i < arr.length; i++) {
             if (arr[i] <= arr[i - 1]) {
                 break;
             }
         }
 
-        // i-1 is the peak
+        
         i--;
 
-        // Peak cannot be first or last
+        
         if (i == 0 || i == arr.length - 1) {
             return false;
         }
 
-        // Decreasing part
+        
         for (; i < arr.length - 1; i++) {
             if (arr[i] <= arr[i + 1]) {
                 return false;
